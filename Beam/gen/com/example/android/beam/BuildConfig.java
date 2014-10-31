7a069633d8dcc6926c6c3e36421ea4305dc29a36
Lokesh Basu <lokesh.basu@gmail.com>
2014-10-31 19:43:42 +0530

    test run

--- Beam/gen/com/example/android/beam/BuildConfig.java
+++ Beam/gen/com/example/android/beam/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package info.androidhive.slidingmenu;
+package com.example.android.beam;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- KeyChainDemoActivity/gen/com/example/android/keychain/BuildConfig.java
+++ KeyChainDemoActivity/gen/com/example/android/keychain/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package info.androidhive.slidingmenu;
+package com.example.android.keychain;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
