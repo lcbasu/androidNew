e19b684e23793053909ea120089ee169efd6a889
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-18 20:01:04 +0530

    adds the gl surface view class

---
+++ OpenGLES/src/com/basu/opengles/MySurfaceView.java
@@ -0,0 +1,21 @@
+package com.basu.opengles;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class MySurfaceView extends GLSurfaceView {
+
+	public MySurfaceView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
