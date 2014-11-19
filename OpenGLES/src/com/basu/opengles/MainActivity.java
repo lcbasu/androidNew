0870b23a6f17d34af12da92d87543a2592076548
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-19 17:55:07 +0530

    completes the surface view class

--- OpenGLES/src/com/basu/opengles/MainActivity.java
+++ OpenGLES/src/com/basu/opengles/MainActivity.java
@@ -3,6 +3,10 @@ package com.basu.opengles;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -12,7 +16,23 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		mGLSurfaceView = new GLSurfaceView(this);
+		
+		//checks if the system supports OpenGL ES 2.0
+		
+		final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+		final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+		final boolean supportsES2 = configurationInfo.reqGlEsVersion >= 0x20000;
+		
+		if (supportsES2) {
+			mGLSurfaceView.setEGLContextClientVersion(2);
+			mGLSurfaceView.setRenderer(new MyRenderer());
+		} else {
+			Log.e("### Version", "OpenGL ES version is less than 2");
+		}
+		
+		setContentView(mGLSurfaceView);
 	}
 
 	@Override
@@ -20,6 +40,18 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mGLSurfaceView.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mGLSurfaceView.onResume();
 	}
 
 }
