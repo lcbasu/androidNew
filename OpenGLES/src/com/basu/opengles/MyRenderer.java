2336c54773869d41fd387f9595d7c3c9a2e6fda8
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-20 21:50:06 +0530

    creates the renderer class

---
+++ OpenGLES/src/com/basu/opengles/MyRenderer.java
@@ -0,0 +1,28 @@
+package com.basu.opengles;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+public class MyRenderer implements Renderer {
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
