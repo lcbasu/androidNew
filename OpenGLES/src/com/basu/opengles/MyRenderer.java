99284c423066c510744b63623bbdc383a11e54f9
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-21 20:12:06 +0530

    renderer imports

--- OpenGLES/src/com/basu/opengles/MyRenderer.java
+++ OpenGLES/src/com/basu/opengles/MyRenderer.java
@@ -3,26 +3,22 @@ package com.basu.opengles;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.GLSurfaceView;
 
-public class MyRenderer implements Renderer {
+public class MyRenderer implements GLSurfaceView.Renderer {
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
