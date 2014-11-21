23c1779189af8dcd99ae6630d4a60ae3e011b228
Lokesh Basu <lokesh.basu@gmail.com>
2014-11-21 20:29:36 +0530

    defining triangle vertices

--- OpenGLES/src/com/basu/opengles/MyRenderer.java
+++ OpenGLES/src/com/basu/opengles/MyRenderer.java
@@ -1,11 +1,65 @@
 package com.basu.opengles;
 
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLSurfaceView;
 
 public class MyRenderer implements GLSurfaceView.Renderer {
+
+	private FloatBuffer mTriangle1Vertices;
+	private FloatBuffer mTriangle2Vertices;
+	private FloatBuffer mTriangle3Vertices;
+
+	/* Bytes per float */
+	private final int mBytesPerFloat = 4;
+
+	public MyRenderer() {
+
+		/* Define the coordinates of the triangles */
+
+
+		/* This triangle is red, green, and blue.*/
+		final float[] triangle1VerticesData = {
+				/* X, Y, Z, */ 
+				/* R, G, B, A */
+				-0.5f, -0.25f, 0.0f, 
+				1.0f, 0.0f, 0.0f, 1.0f,
+
+				0.5f, -0.25f, 0.0f,
+				0.0f, 0.0f, 1.0f, 1.0f,
+
+				0.0f, 0.559016994f, 0.0f, 
+				0.0f, 1.0f, 0.0f, 1.0f};
+
+		/* This triangle is yellow, cyan, and magenta. */
+		final float[] triangle2VerticesData = {
+				/* X, Y, Z, */ 
+				/* R, G, B, A */
+				-0.5f, -0.25f, 0.0f, 
+				1.0f, 1.0f, 0.0f, 1.0f,
+
+				0.5f, -0.25f, 0.0f, 
+				0.0f, 1.0f, 1.0f, 1.0f,
+
+				0.0f, 0.559016994f, 0.0f, 
+				1.0f, 0.0f, 1.0f, 1.0f};
+
+		/* This triangle is white, gray, and black. */
+		final float[] triangle3VerticesData = {
+				/* X, Y, Z, */ 
+				/* R, G, B, A */
+				-0.5f, -0.25f, 0.0f, 
+				1.0f, 1.0f, 1.0f, 1.0f,
+
+				0.5f, -0.25f, 0.0f, 
+				0.5f, 0.5f, 0.5f, 1.0f,
+
+				0.0f, 0.559016994f, 0.0f, 
+				0.0f, 0.0f, 0.0f, 1.0f};
+	}
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
