854ad26d6b1b89fb89dc7a14304df4dc8ba2b21d
Lokesh Basu <lokesh.basu@gmail.com>
2014-12-01 20:43:02 +0530

    fix the vertex shader error

--- OpenGLES/src/com/basu/opengles/MyRenderer.java
+++ OpenGLES/src/com/basu/opengles/MyRenderer.java
@@ -237,16 +237,37 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 
 		/* Shader */
 
-		final String vertexShader = 
-				"uniform mat4 u_MVPMatrix					\n"		/* A constant representing the combined Model-View-Projection matrix. */
-				+	"attribute vec4 a_Position					\n"		/* Per-vertex position information that will be passed in by us. */
-				+	"attribute vec4 a_Color						\n"		/* Per-vertex color information that will be passed in by us. */
-				+	"varying vec4 v_Color						\n"		/* This variable will be passed into the fragment shader.*/
-				+	"void main()								\n"		/* Entry point for the vertex shader. */
-				+	"{											\n"		
-				+	"	v_Color = a_Color						\n"		/* Passing our color information to the fragment shader. It will be interpolated across the triangle.*/
-				+	"	gl_Position = u_MVPMatrix * a_Posotion;	\n"		/* gl_position is an inbuilt variable which is used to store the actual position. */
-				+	"}											\n";	/* We obtain the final position by multiplying the vertex by the matrix to get the final point in normalized screen coordinates. */
+//		final String vertexShader = 
+//				"uniform mat4 u_MVPMatrix;      				\n"		/* A constant representing the combined Model-View-Projection matrix. */
+//				+	"attribute vec4 a_Position;					\n"		/* Per-vertex position information that will be passed in by us. */
+//				+	"attribute vec4 a_Color;					\n"		/* Per-vertex color information that will be passed in by us. */
+//
+//				+	"varying vec4 v_Color;						\n"		/* This variable will be passed into the fragment shader.*/
+//
+//				+	"void main()								\n"		/* Entry point for the vertex shader. */
+//				+	"{											\n"		
+//				+	"	v_Color = a_Color;						\n"		/* Passing our color information to the fragment shader. It will be interpolated across the triangle.*/
+//
+//				+	"	gl_Position = u_MVPMatrix * a_Posotion;	\n"		/* gl_position is an inbuilt variable which is used to store the actual position. */
+//
+//				+	"}											\n";	/* We obtain the final position by multiplying the vertex by the matrix to get the final point in normalized screen coordinates. */
+
+		final String vertexShader =
+				"uniform mat4 u_MVPMatrix;      \n"		// A constant representing the combined model/view/projection matrix.
+
+				+ "attribute vec4 a_Position;     \n"		// Per-vertex position information we will pass in.
+				+ "attribute vec4 a_Color;        \n"		// Per-vertex color information we will pass in.			  
+
+				+ "varying vec4 v_Color;          \n"		// This will be passed into the fragment shader.
+
+				+ "void main()                    \n"		// The entry point for our vertex shader.
+				+ "{                              \n"
+				+ "   v_Color = a_Color;          \n"		// Pass the color through to the fragment shader. 
+				// It will be interpolated across the triangle.
+				+ "   gl_Position = u_MVPMatrix   \n" 	// gl_Position is a special variable used to store the final position.
+				+ "               * a_Position;   \n"     // Multiply the vertex by the matrix to get the final point in 			                                            			 
+				+ "}                              \n";    // normalized screen coordinates.
+
 
 		/*
 		 * The fragment shader actually put stuff on the screen. 
@@ -255,7 +276,7 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 		 */
 
 		final String fragmentShader = 
-				"uniform mat4 u_MVPMatrix						\n"		/* Set the default precision to medium. We don't need as high of a precision in the fragment shader. */
+				"precision mediump float;						\n"		/* Set the default precision to medium. We don't need as high of a precision in the fragment shader. */
 				+	"varying vec4 v_Color;						\n"		/* Color from the vertex shader interpolated across the triangle per fragment. */
 				+	"void main()								\n"		/* Entry point for the fragment shader. */
 				+	"{											\n"		
