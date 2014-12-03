f0d34e3d9cfb24399fe02f23fbd74d307aa6e3b1
Lokesh Basu <lokesh.basu@gmail.com>
2014-12-03 20:23:44 +0530

    Initialize the cube model data.

--- OpenGLES_L2/src/com/basu/opengles_l2/MyRenderer.java
+++ OpenGLES_L2/src/com/basu/opengles_l2/MyRenderer.java
@@ -54,60 +54,179 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 	 */
 	private float[] mModelMatrix = new float[16];
 
-	public MyRenderer() {
+	/*
+	 * Initialize the model data.
+	 */
+	public MyRenderer()
+	{	
+		// Define points for a cube.		
 
-		/* Define the coordinates of the triangles */
+		// X, Y, Z
+		final float[] cubePositionData =
+			{
+				/* In OpenGL counter-clockwise winding is default. This means that when we look at a triangle, 
+				 *  if the points are counter-clockwise we are looking at the "front". If not we are looking at
+				 * the back. OpenGL has an optimization where all back-facing triangles are culled, since they
+				 * usually represent the back side of an object and aren't visible anyways.
+				 */
 
+				/* Front face */
+				-1.0f, 1.0f, 1.0f,				
+				-1.0f, -1.0f, 1.0f,
+				1.0f, 1.0f, 1.0f, 
+				-1.0f, -1.0f, 1.0f, 				
+				1.0f, -1.0f, 1.0f,
+				1.0f, 1.0f, 1.0f,
 
-		/* This triangle is red, green, and blue.*/
-		final float[] triangle1VerticesData = {
-				/* X, Y, Z, */ 
-				/* R, G, B, A */
-				-0.5f, -0.25f, 0.0f, 
+				/* Right face */
+				1.0f, 1.0f, 1.0f,				
+				1.0f, -1.0f, 1.0f,
+				1.0f, 1.0f, -1.0f,
+				1.0f, -1.0f, 1.0f,				
+				1.0f, -1.0f, -1.0f,
+				1.0f, 1.0f, -1.0f,
+
+				/* Back face */
+				1.0f, 1.0f, -1.0f,				
+				1.0f, -1.0f, -1.0f,
+				-1.0f, 1.0f, -1.0f,
+				1.0f, -1.0f, -1.0f,				
+				-1.0f, -1.0f, -1.0f,
+				-1.0f, 1.0f, -1.0f,
+
+				/* Left face */
+				-1.0f, 1.0f, -1.0f,				
+				-1.0f, -1.0f, -1.0f,
+				-1.0f, 1.0f, 1.0f, 
+				-1.0f, -1.0f, -1.0f,				
+				-1.0f, -1.0f, 1.0f, 
+				-1.0f, 1.0f, 1.0f, 
+
+				/* Top face */
+				-1.0f, 1.0f, -1.0f,				
+				-1.0f, 1.0f, 1.0f, 
+				1.0f, 1.0f, -1.0f, 
+				-1.0f, 1.0f, 1.0f, 				
+				1.0f, 1.0f, 1.0f, 
+				1.0f, 1.0f, -1.0f,
+
+				/* Bottom face */
+				1.0f, -1.0f, -1.0f,				
+				1.0f, -1.0f, 1.0f, 
+				-1.0f, -1.0f, -1.0f,
+				1.0f, -1.0f, 1.0f, 				
+				-1.0f, -1.0f, 1.0f,
+				-1.0f, -1.0f, -1.0f,
+			};	
+
+		/* R, G, B, A */
+		final float[] cubeColorData =
+			{				
+				/* Front face (red) */
+				1.0f, 0.0f, 0.0f, 1.0f,				
+				1.0f, 0.0f, 0.0f, 1.0f,
+				1.0f, 0.0f, 0.0f, 1.0f,
+				1.0f, 0.0f, 0.0f, 1.0f,				
+				1.0f, 0.0f, 0.0f, 1.0f,
 				1.0f, 0.0f, 0.0f, 1.0f,
 
-				0.5f, -0.25f, 0.0f,
+				/* Right face (green) */
+				0.0f, 1.0f, 0.0f, 1.0f,				
+				0.0f, 1.0f, 0.0f, 1.0f,
+				0.0f, 1.0f, 0.0f, 1.0f,
+				0.0f, 1.0f, 0.0f, 1.0f,				
+				0.0f, 1.0f, 0.0f, 1.0f,
+				0.0f, 1.0f, 0.0f, 1.0f,
+
+				/* Back face (blue) */
+				0.0f, 0.0f, 1.0f, 1.0f,				
+				0.0f, 0.0f, 1.0f, 1.0f,
+				0.0f, 0.0f, 1.0f, 1.0f,
+				0.0f, 0.0f, 1.0f, 1.0f,				
+				0.0f, 0.0f, 1.0f, 1.0f,
 				0.0f, 0.0f, 1.0f, 1.0f,
 
-				0.0f, 0.559016994f, 0.0f, 
-				0.0f, 1.0f, 0.0f, 1.0f};
-
-		/* This triangle is yellow, cyan, and magenta. */
-		final float[] triangle2VerticesData = {
-				/* X, Y, Z, */ 
-				/* R, G, B, A */
-				-0.5f, -0.25f, 0.0f, 
+				/* Left face (yellow) */
+				1.0f, 1.0f, 0.0f, 1.0f,				
+				1.0f, 1.0f, 0.0f, 1.0f,
+				1.0f, 1.0f, 0.0f, 1.0f,
+				1.0f, 1.0f, 0.0f, 1.0f,				
+				1.0f, 1.0f, 0.0f, 1.0f,
 				1.0f, 1.0f, 0.0f, 1.0f,
 
-				0.5f, -0.25f, 0.0f, 
+				/* Top face (cyan) */
+				0.0f, 1.0f, 1.0f, 1.0f,				
+				0.0f, 1.0f, 1.0f, 1.0f,
+				0.0f, 1.0f, 1.0f, 1.0f,
+				0.0f, 1.0f, 1.0f, 1.0f,				
+				0.0f, 1.0f, 1.0f, 1.0f,
 				0.0f, 1.0f, 1.0f, 1.0f,
 
-				0.0f, 0.559016994f, 0.0f, 
-				1.0f, 0.0f, 1.0f, 1.0f};
+				/* Bottom face (magenta) */
+				1.0f, 0.0f, 1.0f, 1.0f,				
+				1.0f, 0.0f, 1.0f, 1.0f,
+				1.0f, 0.0f, 1.0f, 1.0f,
+				1.0f, 0.0f, 1.0f, 1.0f,				
+				1.0f, 0.0f, 1.0f, 1.0f,
+				1.0f, 0.0f, 1.0f, 1.0f
+			};
 
-		/* This triangle is white, gray, and black. */
-		final float[] triangle3VerticesData = {
-				/* X, Y, Z, */ 
-				/* R, G, B, A */
-				-0.5f, -0.25f, 0.0f, 
-				1.0f, 1.0f, 1.0f, 1.0f,
+		/* X, Y, Z */
+		/* The normal is used in light calculations and is a vector which points
+		 * 
+		 * orthogonal to the plane of the surface. For a cube model, the normals
+		 * should be orthogonal to the points of each face. 
+		 */
+		final float[] cubeNormalData =
+			{												
+				/* Front face */
+				0.0f, 0.0f, 1.0f,				
+				0.0f, 0.0f, 1.0f,
+				0.0f, 0.0f, 1.0f,
+				0.0f, 0.0f, 1.0f,				
+				0.0f, 0.0f, 1.0f,
+				0.0f, 0.0f, 1.0f,
 
-				0.5f, -0.25f, 0.0f, 
-				0.5f, 0.5f, 0.5f, 1.0f,
+				/* Right face */
+				1.0f, 0.0f, 0.0f,				
+				1.0f, 0.0f, 0.0f,
+				1.0f, 0.0f, 0.0f,
+				1.0f, 0.0f, 0.0f,				
+				1.0f, 0.0f, 0.0f,
+				1.0f, 0.0f, 0.0f,
 
-				0.0f, 0.559016994f, 0.0f, 
-				0.0f, 0.0f, 0.0f, 1.0f};
+				/* Back face */ 
+				0.0f, 0.0f, -1.0f,				
+				0.0f, 0.0f, -1.0f,
+				0.0f, 0.0f, -1.0f,
+				0.0f, 0.0f, -1.0f,				
+				0.0f, 0.0f, -1.0f,
+				0.0f, 0.0f, -1.0f,
 
-		/* Initialize the buffers so that Java stores the bytes as native side order */
-		/* This is done because the OpenGL is implemented in C so we need to do so*/
-		mTriangle1Vertices = ByteBuffer.allocateDirect(triangle1VerticesData.length * mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
-		mTriangle2Vertices = ByteBuffer.allocateDirect(triangle2VerticesData.length * mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
-		mTriangle3Vertices = ByteBuffer.allocateDirect(triangle3VerticesData.length * mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
+				/* Left face */
+				-1.0f, 0.0f, 0.0f,				
+				-1.0f, 0.0f, 0.0f,
+				-1.0f, 0.0f, 0.0f,
+				-1.0f, 0.0f, 0.0f,				
+				-1.0f, 0.0f, 0.0f,
+				-1.0f, 0.0f, 0.0f,
 
-		/* Here we copy our array into the buffer */
-		mTriangle1Vertices.put(triangle1VerticesData).position(0);
-		mTriangle2Vertices.put(triangle2VerticesData).position(0);
-		mTriangle3Vertices.put(triangle3VerticesData).position(0);
+				/* Top face */ 
+				0.0f, 1.0f, 0.0f,			
+				0.0f, 1.0f, 0.0f,
+				0.0f, 1.0f, 0.0f,
+				0.0f, 1.0f, 0.0f,				
+				0.0f, 1.0f, 0.0f,
+				0.0f, 1.0f, 0.0f,
+
+				/* Bottom face */ 
+				0.0f, -1.0f, 0.0f,			
+				0.0f, -1.0f, 0.0f,
+				0.0f, -1.0f, 0.0f,
+				0.0f, -1.0f, 0.0f,				
+				0.0f, -1.0f, 0.0f,
+				0.0f, -1.0f, 0.0f
+			};
 	}
 
 	@Override
