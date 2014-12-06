2069417da3de5866528c216ebcee1c85666ed9b4
Lokesh Basu <lokesh.basu@gmail.com>
2014-12-06 09:47:01 +0530

    vertex and fragment shader

--- OpenGLES_L2/src/com/basu/opengles_l2/MyRenderer.java
+++ OpenGLES_L2/src/com/basu/opengles_l2/MyRenderer.java
@@ -14,12 +14,12 @@ import android.opengl.Matrix;
 import android.os.SystemClock;
 
 public class MyRenderer implements GLSurfaceView.Renderer {
-	
+
 	/* Store our model data in a float buffer. */
 	private final FloatBuffer mCubePositions;
 	private final FloatBuffer mCubeColors;
 	private final FloatBuffer mCubeNormals;
-	
+
 	/* Bytes per float */
 	private final int mBytesPerFloat = 4;
 
@@ -228,17 +228,71 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 				0.0f, -1.0f, 0.0f,
 				0.0f, -1.0f, 0.0f
 			};
-		
+
 		/* Initialize the buffers. */
 		mCubePositions = ByteBuffer.allocateDirect(cubePositionData.length * mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();
 		mCubePositions.put(cubePositionData).position(0);		
-		
+
 		mCubeColors = ByteBuffer.allocateDirect(cubeColorData.length * mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();							
 		mCubeColors.put(cubeColorData).position(0);
-		
+
 		mCubeNormals = ByteBuffer.allocateDirect(cubeNormalData.length * mBytesPerFloat).order(ByteOrder.nativeOrder()).asFloatBuffer();							
 		mCubeNormals.put(cubeNormalData).position(0);
 	}
+
+
+	/* Vertex shader. */
+	protected String getVertexShader()
+	{
+		final String vertexShader =
+					"uniform mat4 u_MVPMatrix;      \n"		/* A constant representing the combined model/view/projection matrix. */				+ 	"uniform mat4 u_MVMatrix;       \n"		// A constant representing the combined model/view matrix.	
+				+ 	"uniform vec3 u_LightPos;       \n"	    /* The position of the light in eye space. */
+				+ 	"attribute vec4 a_Position;     \n"		/* Per-vertex position information we will pass in. */
+				+ 	"attribute vec4 a_Color;        \n"		/* Per-vertex color information we will pass in. */
+				+ 	"attribute vec3 a_Normal;       \n"		/* Per-vertex normal information we will pass in. */
+				+ 	"varying vec4 v_Color;          \n"		/* This will be passed into the fragment shader. */
+				+ 	"void main()                    \n" 	/* The entry point for our vertex shader. */
+				+ 	"{                              \n"		
+				/* Transform the vertex into eye space. */
+				+ 	"   vec3 modelViewVertex = vec3(u_MVMatrix * a_Position);              \n"
+				/* Transform the normal's orientation into eye space.*/
+				+ 	"   vec3 modelViewNormal = vec3(u_MVMatrix * vec4(a_Normal, 0.0));     \n"
+				/* Will be used for attenuation.*/
+				+ 	"   float distance = length(u_LightPos - modelViewVertex);             \n"
+				/* Get a lighting direction vector from the light to the vertex.*/
+				+ 	"   vec3 lightVector = normalize(u_LightPos - modelViewVertex);        \n"
+				/* Calculate the dot product of the light vector and vertex normal. 
+				 * If the normal and light vector are pointing in the same direction then it will get max illumination.
+				 */
+				+ 	"   float diffuse = max(dot(modelViewNormal, lightVector), 0.1);       \n" 	  		  													  
+				/* Attenuate the light based on distance.*/
+				+ 	"   diffuse = diffuse * (1.0 / (1.0 + (0.25 * distance * distance)));  \n"
+				/* Multiply the color by the illumination level. It will be interpolated across the triangle.*/
+				+ 	"   v_Color = a_Color * diffuse;                                       \n" 	 
+				/* gl_Position is a special variable used to store the final position. 
+				 * Multiply the vertex by the matrix to get the final point in normalized screen coordinates.
+				 */
+				+ 	"   gl_Position = u_MVPMatrix * a_Position;                            \n"     
+				+ 	"}                                                                     \n"; 
+
+		return vertexShader;
+	}
+
+	
+	/* Fragment shader. */
+	protected String getFragmentShader()
+	{
+		final String fragmentShader =
+					"precision mediump float;       \n"		/* Set the default precision to medium. We don't need as high of a precision in the fragment shader. */
+				+ 	"varying vec4 v_Color;          \n"		/* This is the color from the vertex shader interpolated across the triangle per fragment. */
+				+ 	"void main()                    \n"		/* The entry point for our fragment shader. */
+				+ 	"{                              \n"
+				+ 	"   gl_FragColor = v_Color;     \n"		/* Pass the color directly through the pipeline. */		  
+				+ 	"}                              \n";
+
+		return fragmentShader;
+	}
+
 
 	@Override
 	public void onDrawFrame(GL10 gl) {
